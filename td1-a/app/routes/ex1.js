533112363ca4e76f4501f335051c9522eba258ef
Nicolas Barbet <21800428@etu.unicaen.fr>
2020-02-12 02:43:42 +0100

    Déplacement TD1 vers la branche dédiée

--- td1-a/app/routes/ex1.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-import Note from 'td1/app/utils/note-class';
-
-export default Route.extend({
-    model(){
-        //return Note.create();
-       return {nom:'Donald Trump', details:'Détails du client Trump ...'};
-    }
-});
