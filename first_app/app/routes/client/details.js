c35b9b4673b9d42ccfdf92f3b22fb4d35e11482f
Nicolas Barbet <21800428@etu.unicaen.fr>
2020-02-12 02:52:05 +0100

    Suppression TD1

--- first_app/app/routes/client/details.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-    actions:{
-        close(){
-            this.transitionTo("client");
-        }
-    }
-});
--- td2/app/helpers/array-contains.js
+++
@@ -1,8 +0,0 @@
-import {helper} from '@ember/component/helper';
- 
-export function arrayContains(params) {
-  const [items, value] = params;
-  return items.includes(value);
-}
- 
-export default helper(arrayContains);
