cff68ce07cef8b97b16dd67655419339d9efcacd
Nicolas <21800428@etu.unicaen.fr>
2020-02-12 03:01:34 +0100

    Projet vidé

--- first_app/app/routes/client.js
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-    model(){
-        return {nom:'Donald Trump', details:'Détails du client Trump ...'};
-    }
-});
--- td1/app/routes/ex1.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-import Note from 'td1/utils/note-class';
-
-export default Route.extend({
-    model(){
-        return Note.create();
-    }
-});
