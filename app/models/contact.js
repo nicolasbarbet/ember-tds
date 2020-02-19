fbe0a4cafcf96b87c747582e8f8043aeb7ab5388
Nicolas Barbet <21800428@etu.unicaen.fr>
2020-02-19 15:08:13 +0100

    Propriétés de Contacts OK

--- app/models/contact.js
+++ app/models/contact.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
-
+    nom : DS.attr('string'),
+    prenom : DS.attr('string'),
+    email : DS.attr('string')
 });
