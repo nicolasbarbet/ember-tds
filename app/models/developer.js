d73631813e9867301e5de1a4cbf2023fcea97cb1
Nicolas Barbet <21800428@etu.unicaen.fr>
2020-03-29 18:01:43 +0200

    Modification du model developer et ajout model project

--- app/models/developer.js
+++ app/models/developer.js
@@ -2,5 +2,6 @@ import Model from '@ember-data/model';
 import DS from 'ember-data';
 
 export default Model.extend({
-    identity: DS.attr('string')
+    identity: DS.attr('string'),
+    projects: DS.hasMany('project')
 });
--- app/models/project.js
+++ app/models/project.js
@@ -1,5 +1,11 @@
-import Model from '@ember-data/model';
+import DS from 'ember-data';
+const { Model } = DS;
 
-export default class ProjectModel extends Model {
-
-}
+export default Model.extend({
+    nom : DS.attr('string'),
+    description : DS.attr('string'),
+    datedebut : DS.attr('utc'),
+    datefin : DS.attr('utc'),
+    proprietaire : DS.belongsTo('developer')
+    //stories : DS.hasMany('story')
+});
